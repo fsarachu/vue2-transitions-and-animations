4f5f2245e9bba6bed3e35423e058ba146f55b2da
fsarachu <fsarachu@gmail.com>
2017-04-05 00:17:20 -0300

    Add a fading transition

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,18 @@
                 <hr>
                 <h2 class="subtitle is-3">Transitions</h2>
                 <div class="block">
-                    <button @click="showMessage = !showMessage" class="button is-primary">Toggle Message</button>
+                    <button @click="showMessage = !showMessage" class="button is-primary">Fade Message</button>
                 </div>
-                <article v-if="showMessage" class="message is-primary">
-                    <div class="message-header">
-                        <p>Some Message</p>
-                    </div>
-                    <div class="message-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, sit.
-                    </div>
-                </article>
+                <transition name="fade">
+                    <article v-if="showMessage" class="message is-primary">
+                        <div class="message-header">
+                            <h3>Some Message</h3>
+                        </div>
+                        <p class="message-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, sit.
+                        </p>
+                    </article>
+                </transition>
             </div>
         </section>
     </div>
@@ -32,5 +34,20 @@
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
 
+    .fade-enter-active {
+        transition: opacity .3s ease-in;
+    }
+
+    .fade-leave {
+
+    }
+
+    .fade-leave-active {
+        transition: opacity .3s ease-out;
+        opacity: 0;
+    }
 </style>
